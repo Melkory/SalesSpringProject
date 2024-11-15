e6375706d48c931056d4c3547625da4edee07676
Melkory <carloseduardo.dziombra@hotmail.com>
2024-11-15 16:13:37 -0300

    Consulta de produtos por nome

--- src/main/java/com/dziombra/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/dziombra/dscommerce/repositories/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.dziombra.dscommerce.repositories;
 
 import com.dziombra.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository <Product, Long> {
+
+    @Query("Select obj from Product obj " +
+            "where upper(obj.name) like upper(concat('%', :name, '%'))")
+    Page<Product> searchByName (String name, Pageable pageable);
+
 }
