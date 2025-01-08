abc1bea6a9bd7d59179273ba42f2bb918d1df69f
Melkory <carloseduardo.dziombra@hotmail.com>
2025-01-08 19:43:12 -0300

    Fix validation

--- src/main/java/com/dziombra/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/dziombra/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.dziombra.dscommerce.dto;
 
 import com.dziombra.dscommerce.entities.Category;
 import com.dziombra.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +18,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Precisa ter no mínimo 10 caracteres!")
     private String description;
 
+    @NotNull(message = "Campo requerido!")
     @Positive(message = "O Preço precisa ser positivo!")
     private Double price;
     private String imgUrl;
